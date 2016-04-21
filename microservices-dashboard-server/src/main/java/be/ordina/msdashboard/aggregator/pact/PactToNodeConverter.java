75f40a203a9d8ad434639da6db33a56f05472923
andreasevers <andreas.evers@ordina.be>
2016-04-21 18:28:56 +0200

    Added rel protocol descriptor

--- microservices-dashboard-server/src/main/java/be/ordina/msdashboard/aggregator/pact/PactToNodeConverter.java
+++ microservices-dashboard-server/src/main/java/be/ordina/msdashboard/aggregator/pact/PactToNodeConverter.java
@@ -5,7 +5,13 @@ import be.ordina.msdashboard.model.NodeBuilder;
 import com.jayway.jsonpath.JsonPath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.EmbeddedValueResolverAware;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringValueResolver;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,19 +23,20 @@ public class PactToNodeConverter {
 	private static final Logger LOG = LoggerFactory.getLogger(PactToNodeConverter.class);
 	private static final String UI_COMPONENT = "UI_COMPONENT";
 	private static final String UP = "UP";
+	private static final String relPath = "rel://";
 
 	public Node convert(final String source, final String pactUrl) {
-		List<String> rels = JsonPath.read(source, "$.interactions[*].request.path");
+		List<String> paths = JsonPath.read(source, "$.interactions[*].request.path");
 		String provider = JsonPath.read(source, "$.provider.name");
 		String consumer = JsonPath.read(source, "$.consumer.name");
 
-		LOG.debug("Retrieved UI Component for consumer {} and producer {} with rels {}", consumer, provider, rels);
+		LOG.debug("Retrieved UI Component for consumer {} and producer {} with rels {}", consumer, provider, paths);
 
 		NodeBuilder node = new NodeBuilder();
 		node.withId(consumer);
 		node.withLane(0);
-		rels.stream().forEach(rel -> {
-				Node linkNode = NodeBuilder.node().withId(rel).build();
+		paths.stream().forEach(path -> {
+				Node linkNode = NodeBuilder.node().withId(convertPathToRel(path)).build();
 				node.withLinkedNode(linkNode);
 		});
 		Map<String, Object> details = new HashMap<>();
@@ -40,6 +47,17 @@ public class PactToNodeConverter {
 		node.havingDetails(details);
 
 		return node.build();
+	}
+
+	private String convertPathToRel(String path) {
+		String rel = "";
+		if (path.startsWith(relPath) || path.startsWith("/" + relPath)) {
+			rel = path.substring(path.indexOf(relPath) + relPath.length());
+		} else {
+			rel = path;
+		}
+		LOG.debug("Path '{}' resolved to rel '{}'", path, rel);
+		return rel;
 	}
 }
 
